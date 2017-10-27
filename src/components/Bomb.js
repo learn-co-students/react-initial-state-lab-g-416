23279d5dbf0cdc3702dbfdeb54c5acc98efe7b79
Han Lee <hanlee12@gmail.com>
2017-10-27 14:02:41 -0400

    done

--- src/components/Bomb.js
+++ src/components/Bomb.js
@@ -1,1 +1,21 @@
 // Bomb Component Code Goes Here
+import React from 'react';
+
+class Bomb extends React.Component {
+  constructor(props) {
+    super();
+
+    this.state = {
+      secondsLeft: props.initialCount
+    }
+  }
+  render() {
+    return (
+      <div>
+        {this.state.secondsLeft === 0 ? 'Boom!' : this.state.secondsLeft + ' seconds left before I go boom!'}
+      </div>
+    )
+  }
+}
+
+export default Bomb;
--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -1,1 +1,19 @@
-// Bomb Component Code Goes Here
+// ImageSlider Component Code Goes Here
+import React from 'react';
+
+class ImageSlider extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      currentSlideIndex: 0
+    }
+  }
+  render() {
+    return (
+      <p>I am on slide {this.state.currentSlideIndex}</p>
+    )
+  }
+}
+
+export default ImageSlider;
